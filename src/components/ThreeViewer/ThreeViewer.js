8b6f389882edc95caff0d2f7a0435e4be61a1e18
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2018-06-08 18:52:04 +0900

    Revert "implemented parsing MultiLayeredGraph (#2)" (#3)
    This reverts commit 9c1045255548e8b227494950def000aaceb5b514.

--- src/components/ThreeViewer/ThreeViewer.js
+++ src/components/ThreeViewer/ThreeViewer.js
@@ -18,7 +18,6 @@ export default class ThreeViewer {
     this.scene = new THREE.Scene()
     this.scene.name = 'Main Scene'
 
-    // scene helper for 
     this.sceneHelper = new THREE.Scene()
     this.sceneHelper.name = 'Scene Helpers'
 
