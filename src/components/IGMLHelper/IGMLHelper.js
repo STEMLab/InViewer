f4531c77940299d9b72f1b5ff7a66087dbfa30d9
Hyung-Gyu Ryoo <hyunggyu.ryoo@gmail.com>
2018-06-08 18:51:43 +0900

    Revert "implemented parsing MultiLayeredGraph (#2)"
    This reverts commit 9c1045255548e8b227494950def000aaceb5b514.

--- src/components/IGMLHelper/IGMLHelper.js
+++ src/components/IGMLHelper/IGMLHelper.js
@@ -11,45 +11,9 @@ export default class IGMLHelper {
     this.allGeometries = {};
     this.information = {};
 
-    this.cellMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff, opacity:1, transparent: true, side: THREE.DoubleSide} );
+    this.cellMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff, opacity:0.3, transparent: true, side: THREE.DoubleSide} );
     this.cbMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff, side: THREE.DoubleSide} );
     this.lineMaterial = new THREE.LineBasicMaterial( {color: 0x000000} );
-  }
-
-  parseCellSpace(indoor) {
-    if(indoor.cells) {
-      var cells = indoor.cells
-      for(var cell of cells) {
-        var cellGeometry = []
-
-        var surfaces = cell.geometry
-        if(surfaces !== undefined) {
-          for(var surface of surfaces) {
-
-          }
-        }
-
-      }
-    }
-  }
-
-  parseCellSpaceBoundary(indoor) {
-    if(indoor.cellBoundaries) {
-      for(var cbs of cellBoundaries) {
-        var geometry
-        if(cbs.geometryType === "3D") {
-
-        } else {
-
-        }
-      }
-    }
-  }
-
-  parseSpaceLayer(indoor) {
-    if(indoor.multiLayeredGraph) {
-
-    }
   }
 
   makeGeometry (indoor) {
@@ -100,7 +64,6 @@ export default class IGMLHelper {
     cellSpaces.name = 'CellSpace';
 
     var cells = indoor.cells;
-
     for(var cell of cells) {
       var key = cell.id;
 
@@ -178,12 +141,6 @@ export default class IGMLHelper {
     primalSpaceFeatures.add( cellSpaces );
     primalSpaceFeatures.add( cellSpaceBoundary );
     group.add(primalSpaceFeatures)
-
-    var mS = (new THREE.Matrix4()).identity();
-    mS.elements[0] = -1;
-    //mS.elements[5] = -1;
-    mS.elements[8] = -1;
-    group.applyMatrix(mS);
 
     return group;
   }
