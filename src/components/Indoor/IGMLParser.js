f747705992336c348c2276b137e9133dd80a8ff1
suheeeee <lalune1120@hotmail.com>
2018-11-27 16:49:42 +0900

    corrected the incorrect merged content.

--- src/components/Indoor/IGMLParser.js
+++ src/components/Indoor/IGMLParser.js
@@ -23,16 +23,16 @@ export default class IGMLParser {
   }
 
   parse(igmlContent) {
-    if(igmlContent == undefined) {
+    if (igmlContent == undefined) {
       log.debug("target IndoorGML is empty")
       return
     }
 
     var primalSpace = igmlContent.value.primalSpaceFeatures
-    if(primalSpace !== undefined) {
+    if (primalSpace !== undefined) {
       var cells = primalSpace.primalSpaceFeatures.cellSpaceMember
       if (cells !== undefined) {
-        for(var cell of cells) {
+        for (var cell of cells) {
           var c = new CellSpace()
           c.fromJSON(cell.cellSpace, this)
           this.cells.push(c)
@@ -41,38 +41,39 @@ export default class IGMLParser {
 
       var cellboundarys = primalSpace.primalSpaceFeatures.cellSpaceBoundaryMember
       if (cellboundarys !== undefined) {
-        for(var cellboundary of cellboundarys) {
+        for (var cellboundary of cellboundarys) {
           var cb = new CellSpaceBoundary()
           cb.fromJSON(cellboundary.cellSpaceBoundary, this)
           this.cellBoundaries.push(cb)
         }
       }
     }
-    
-	if( igmlContent.value.multiLayeredGraph !== undefined){
-		var mg = igmlContent.value.multiLayeredGraph.multiLayeredGraph;
-		var layers = mg.spaceLayers;
-		if( typeof layers !== 'undefined') {
-		  for(var layer of layers) {
-			var layerMember = layer.spaceLayerMember
-			for(var member of layerMember) {
-			  var spaceLayer = member.spaceLayer
-			  var spaceLayerId = spaceLayer.id
 
-			  var sl = new SpaceLayer();
-			  sl.fromJSON(spaceLayer, this);
-			  this.multiLayeredGraph.push(sl);
-			}
-		  }
-		}
+    if (igmlContent.value.multiLayeredGraph !== undefined) {
+      var mg = igmlContent.value.multiLayeredGraph.multiLayeredGraph;
+      var layers = mg.spaceLayers;
+      if (typeof layers !== 'undefined') {
+        for (var layer of layers) {
+          var layerMember = layer.spaceLayerMember
+          for (var member of layerMember) {
+            var spaceLayer = member.spaceLayer
+            var spaceLayerId = spaceLayer.id
+
+            var sl = new SpaceLayer();
+            sl.fromJSON(spaceLayer, this);
+            this.multiLayeredGraph.push(sl);
+          }
+        }
+      }
+    }
   }
 
   parseNodes(nodesArr, spaceLayer) {
-    if(nodesArr !== undefined) {
-      for(var nodes of nodesArr) {
+    if (nodesArr !== undefined) {
+      for (var nodes of nodesArr) {
         var stateMembers = nodes.stateMember;
-        if(stateMembers !== undefined) {
-          for(var stateMember of stateMembers) {
+        if (stateMembers !== undefined) {
+          for (var stateMember of stateMembers) {
             var newState = new State();
             newState.fromJSON(stateMember.state, this)
             spaceLayer.nodes.push(newState)
@@ -83,11 +84,11 @@ export default class IGMLParser {
   }
 
   parseEdges(edgeArr, spaceLayer) {
-    if(edgeArr !== undefined) {
-      for(var edges of edgeArr) {
+    if (edgeArr !== undefined) {
+      for (var edges of edgeArr) {
         var transitionMembers = edges.transitionMember;
-        if(transitionMembers !== undefined) {
-          for(var transitionMember of transitionMembers) {
+        if (transitionMembers !== undefined) {
+          for (var transitionMember of transitionMembers) {
             var newTransition = new Transition();
             newTransition.fromJSON(transitionMember.transition, this)
             spaceLayer.edges.push(newTransition)
@@ -98,7 +99,7 @@ export default class IGMLParser {
   }
 
   parsePosOrPointPropertyOrPointRep(points, target) {
-    if(points.value !== undefined) {
+    if (points.value !== undefined) {
       var point = [points.value[0], points.value[2], -points.value[1]];
       target.push(point[0], point[1], point[2]);
       this.setMinMax(point)
